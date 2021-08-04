75d12204dcf37b54a582c00c57fda64ae65a9f81
Carlos Martinez <curlosmart@gmail.com>
2021-08-04 13:40:24 -0400

    grid can be generated of different sizes and fill

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-const gridElem = document.querySelector('.grid')
-console.log(gridElem)
-console.log(gridElem.style)
-console.log(gridElem.style.gridTemplateColumns)
-gridElem.style['gridTemplateColumns'] = 'repeat(10, 80px);'
+// const gridElem = document.querySelector('.grid')
+// console.log(gridElem)
+// console.log(gridElem.style)
+// console.log(gridElem.style.gridTemplateColumns)
+// gridElem.style['gridTemplateColumns'] = 'repeat(10, 80px);'
+
+const grid = document.querySelector('.grid')
+
+const generateGrid = (gridSize=10) => {
+    grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`
+
+    for (let i = 0; i < gridSize ** 2; i++) {
+        let cell = document.createElement('div')
+        cell.classList.add('cell')
+        cell.addEventListener("mouseover", changeColor)
+        grid.appendChild(cell)
+    }
+}
+
+const changeColor = (event) => {
+    const elem = event.target
+    elem.style.backgroundColor = 'black'
+}
+
+generateGrid()
